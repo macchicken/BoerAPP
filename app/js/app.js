823e22ce49ca5f9cbb364e6a5c819eade280234f
Barry Chen <log525lg100@googlemail.com>
2015-07-17 13:18:27 +0000

    Changes made by Barry Chen (log525lg100@googlemail.com). add user favorites and store in the local

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,29 @@
 (function () {
     var app;
     var apikey = "8d3fb09eae35f2c1e056e7aee9a9cf84";
-    var kw,pageNum=0,searched=false;
+    var kw, pageNum = 0,
+        searched = false;
     var fields = "tags,url_sq,owner_name,views";
     var patt1 = new RegExp("\\w+");
     window.Photos = {
         data: new kendo.data.DataSource({
             transport: {
                 read: {
-                    url: function(){
-                    	var keywd;
-                        if (kw==null||kw===""){keywd="apple";}
-                        else{keywd=kw;}
-                        if (pageNum<0){pageNum=0;}
-                        if (!searched){pageNum=pageNum+1;}
-                        searched=false;
-                        return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + apikey +"&tags=" + keywd +"&extras=" + fields + "&per_page=10&page="+pageNum+"&format=json&nojsoncallback=1";
+                    url: function () {
+                        var keywd;
+                        if (kw == null || kw === "") {
+                            keywd = "apple";
+                        } else {
+                            keywd = kw;
+                        }
+                        if (pageNum < 0) {
+                            pageNum = 0;
+                        }
+                        if (!searched) {
+                            pageNum = pageNum + 1;
+                        }
+                        searched = false;
+                        return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + apikey + "&tags=" + keywd + "&extras=" + fields + "&per_page=10&page=" + pageNum + "&format=json&nojsoncallback=1";
                     },
                     dataType: "json"
                 }
@@ -33,19 +41,79 @@
             app.navigate("views/settings.html");
         },
         search: function () {
-           	var kwt = $.trim($("#searchContent").find("#kwords").val());
-            if (kwt===""){navigator.notification.alert("search key words should not be empty");return;}
-            if (!patt1.test(kwt)){navigator.notification.alert("search key words should be only words");return;}
-            var pageNumT = $("#searchContent").find("#page").val()*1;
-            if (pageNumT<=0||isNaN(pageNumT)){navigator.notification.alert("page number should be bigger than zero.");return;}
-            kw=kwt;pageNum=pageNumT;searched=true;
+            var kwt = $.trim($("#searchContent").find("#kwords").val());
+            if (kwt === "") {
+                navigator.notification.alert("search key words should not be empty");
+                return;
+            }
+            if (!patt1.test(kwt)) {
+                navigator.notification.alert("search key words should be only words");
+                return;
+            }
+            var pageNumT = $("#searchContent").find("#page").val() * 1;
+            if (pageNumT <= 0 || isNaN(pageNumT)) {
+                navigator.notification.alert("page number should be bigger than zero.");
+                return;
+            }
+            kw = kwt;
+            pageNum = pageNumT;
+            searched = true;
             app.navigate("#index");
         }
     };
 
+    window.MyFavorites = {
+        data: new kendo.data.DataSource({
+            pageSize: 10,
+            schema: {
+                model: {
+                    id: "id",
+                    owner: "owner",
+                    title: "title",
+                    views: 0,
+                    tags: "tags",
+                    ownername: "ownername",
+                    url_sql: "url_sql",
+                    height_sq: 75,
+                    width_sq: 75,
+                    favorited: false
+                }
+            }
+        }),
+        show: function() {
+			if (this.data!=null&&this.data!=undefined){
+                this.data.filter({ field: "favorited", operator: "eq", value: false });
+            }
+		},
+		hide: function() {
+			if (this.data!=null&&this.data!=undefined){
+            	this.data.filter([]);
+            }
+		},
+        add: function (photo,favorited) {
+            var tm = this.data.get(photo.id);
+            console.log(photo.id+" favorited add "+favorited);
+            if (tm == null||tm == undefined) {
+                this.data.add({
+                    id: photo.id,
+                    owner: photo.owner,
+                    title: photo.title,
+                    views: photo.views,
+                    tags: photo.tags,
+                    ownername: photo.ownername,
+                    url_sq: photo.url_sq,
+                    height_sq: photo.height_sq,
+                    width_sq: photo.width_sq,
+                    favorited: favorited
+                });
+            }else{
+                tm.set("favorited",favorited);
+            }
+            this.data.sync();
+        }
+    };
     document.addEventListener("deviceready", function () {
         navigator.splashscreen.hide();
-
         app = new kendo.mobile.Application(document.body, {
             layout: "main-layout"
         });
--- app/js/details.js
+++ app/js/details.js
@@ -6,7 +6,6 @@
         var length = imgurl.length;
         return imgurl.substring(0, lasti - 2) + imgurl.substring(lasti, length);
     }
-
     window.PhotoDetail = {
         show: function () {
             //Pull the id number from the query string
@@ -14,17 +13,29 @@
             var idstr = location.substring(location.lastIndexOf('?') + 4);
            
 			currentPhoto=window.Photos.data.get(idstr);
+            if (currentPhoto==null||currentPhoto==undefined){
+                currentPhoto=window.MyFavorites.data.get(idstr);
+            }
             // Create a model for the page and bind it to the view
             var photo = {
                 title: currentPhoto.title + " by " + currentPhoto.ownername,
                 image_url: transferImgUrl(currentPhoto.url_sq),
                 descriptions: currentPhoto.tags
             };
-            kendo.bind($('#photoContent'), photo, kendo.mobile.ui);
             // If the current photo views is bigger than zero,toggle the switch on the view
-            if (currentPhoto.views * 1 >= 1) {
-                $('#favorite').data('kendoMobileSwitch').toggle();
+            // if (currentPhoto.views * 1 >= 1) {
+            //     $('#favorite').data('kendoMobileSwitch').toggle();
+            // }
+            if (window.MyFavorites.data!=null&&window.MyFavorites.data!=undefined){
+                var tm=window.MyFavorites.data.get(idstr);
+                if(tm!=null&&tm!=undefined){
+                    if (tm.favorited){
+                         console.log("PhotoDetail favorited enter "+tm.favorited);
+                        $('#favorite').data('kendoMobileSwitch').toggle();
+                    }
+                }
             }
+            kendo.bind($('#photoContent'), photo, kendo.mobile.ui);
         },
         hide: function () {
 			currentPhoto=null;
@@ -33,6 +44,10 @@
             // Will use the Cordova InAppBrowser plugin when deployed to a device. Opens a new window in the simulator
             var fkpage="https://www.flickr.com/photos/"+currentPhoto.owner+"/"+currentPhoto.id+"/";
             window.open(fkpage, '_blank', 'location=yes');
+        },
+        cphoto: function(){
+            return currentPhoto;
         }
     };
+    
 }());
